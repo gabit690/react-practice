f8f4ccf687a36d49315741f205874bafefe4b43a
gabit <gabit690@gmail.com>
2020-08-25 10:51:56 -0300

    "Componente página"

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 // Importo las dependencias necesarias.
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Card from './components/Card'
+import Excercises from './pages/Excercises'
 import 'bootstrap/dist/css/bootstrap.css'
-import Welcome from './components/Welcome';
 
 const container = document.getElementById('root');
 
-ReactDOM.render(<div>
-                  <Welcome username="Gabriel" />
-                  <Card 
-                    title="Technique Guides"
-                    description="Learn amazing street and calisthenics"
-                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-                    leftColor="#A74CF2"
-                    rightColor="#617BFB"
-                  />
-                </div>, container);
+ReactDOM.render(<Excercises />, container);
