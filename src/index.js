e3275d1f9265f7779e7565c995e58be07d55415a
gabit <gabit690@gmail.com>
2022-05-25 18:48:58 -0300

    Render props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Portal from './components/Portal';
+import Human from './components/RenderProps'
 
-ReactDOM.render(<Portal idContainer="portal-output-2"><h1>Title Children</h1><p>This is a paragraph of the unique child of div 'Portal-output'</p></Portal>, document.getElementById('root'));
+ReactDOM.render(<Human />, document.getElementById('root'));
